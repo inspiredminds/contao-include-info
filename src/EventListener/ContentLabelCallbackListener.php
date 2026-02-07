80360dbf2f1adc82010a1b12e6a60b389f98363c
Sebastian Zoglowek <55794780+zoglo@users.noreply.github.com>
2026-02-07 10:30:34 +0100

    Rewrite the child record callback into a label callback (Contao 5.7 compatibility) (#14)
    * Rewrite the include info to use the label callback instead
    
    * Update the requirements
    
    * Update the backend CSS
    
    * Run the tools
    
    * Remove the `dump()` occurences
    
    * Consider the include element preview as well
    
    * Revert PHP requirement

---
+++ src/EventListener/ContentLabelCallbackListener.php
@@ -0,0 +1,37 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * (c) INSPIRED MINDS
+ */
+
+namespace InspiredMinds\IncludeInfoBundle\EventListener;
+
+use Contao\ContentModel;
+use InspiredMinds\IncludeInfoBundle\Aggregator\IncludesAggregator;
+
+readonly class ContentLabelCallbackListener
+{
+    public function __construct(private IncludesAggregator $aggregator)
+    {
+    }
+
+    public function onLabelCallback(array $row): array
+    {
+        // Render label
+        $label = (new \tl_content())->addCteType($row);
+
+        // Get the content element
+        $element = ContentModel::findById((int) $row['id']);
+
+        // Render the include info for the content element
+        $includeInfo = $this->aggregator->renderIncludesForContentElement($element);
+
+        if ($includeInfo) {
+            $label[1] = $includeInfo.$label[1] ?? '';
+        }
+
+        return $label;
+    }
+}
